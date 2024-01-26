bceeefa97256cad0b2e06ac17ebc6caab8284af3
LeeT <leetrewh@gmail.com>
2024-01-26 12:11:50 +0700

    dalat

--- resources/Da Lat ATV/JS/main.js
+++ resources/Da Lat ATV/JS/main.js
@@ -1,23 +1,3 @@
-// const open = document.getElementById('open');
-// const close = document.getElementById('close');
-// const mblMenu = document.getElementById('mobile-menu');
-
-// function toggleMenu() {
-//     open.classList.toggle('hide');
-//     close.classList.toggle('hide');
-//     mblMenu.classList.toggle('open-menu');
-// }
-
-
-// document.addEventListener('click', function(event) {
-    
-
-//     if (mblMenu.classList.contains('open-menu') && !mblMenu.contains(event.target) && event.target !== open && event.target !== close) {
-//         toggleMenu();
-//     }
-// });
-
-
 function submitForm(event) {
     event.preventDefault();
     
